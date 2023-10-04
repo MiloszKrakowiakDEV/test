b04af544a81b549683c6a1810bac6853dcbd396b
MiloszDEV <miloszz.krakowiak@gmail.com>
2023-10-04 09:14:22 +0200

    dodanie

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(12);
+        int a = 12;
+        int b = 235;
+        System.out.println("To jest " + a + b);
+        System.out.println(a + b + " jest to.");
+        System.out.println( (double) b/ a);
+    }
+}
